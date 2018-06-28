6f87a623ee9b6b88303407b7ce2f7979838a7c0a
eyi1 <ej7729@gmail.com>
2018-06-28 13:55:16 -0400

    add property_type controller and views

--- app/controllers/sub_controllers/property_types_controller.rb
+++ app/controllers/sub_controllers/property_types_controller.rb
@@ -1,2 +1,13 @@
 class PropertyTypesController < ApplicationController
+
+    get '/propertytypes' do 
+       @property_type = PropertyType.all
+       erb :'/property_types/index'
+    end
+
+    get '/propertytypes/:id' do 
+        @property_type = PropertyType.find(params[:id])
+        erb :'/property_types/show'
+    end  
+
 end
